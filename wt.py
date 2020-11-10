3a0523fa9c9111a6358552c751e05560b699c0ea
Krasius <DarkRisingForce@Gmail.com>
2020-11-10 17:04:29 +0200

    First draft

--- wt.py
+++ wt.py
@@ -3,7 +3,8 @@ import configparser
 from datetime import datetime
 
 
-def get_request(url="https://api.openweathermap.org/data/2.5/weather?", q='Beirut', units='metric'):
+def get_request(q='Beirut', units='metric'):
+    url = "https://api.openweathermap.org/data/2.5/weather?"
     cnf = configparser.ConfigParser()
     cnf.read('etc/config.ini')
     appid = cnf.get('main', 'appid')
@@ -12,7 +13,7 @@ def get_request(url="https://api.openweathermap.org/data/2.5/weather?", q='Beiru
     r = requests.get(url, params={'q': q, 'units': units, 'appid': appid,})
     if r.status_code == 200:
         jsn_res = r.json()
-        __filter_response(jsn_res)
+        __filter_response(jsn_res, temp_unit='C' if units == 'metric' else 'K')
     else:
         print('Trouble connecting to server')
 
@@ -26,14 +27,17 @@ def __filter_response(jsn, temp_unit='C'):
     temp_max = jsn['main']['temp_max']
     humidity = jsn['main']['humidity']
     wnd_speed = jsn['wind']['speed']
+    sunrise = datetime.fromtimestamp(jsn['sys']['sunrise']).strftime("%H:%m")
+    sunset = datetime.fromtimestamp(jsn['sys']['sunset']).strftime("%H:%m")
 
     res = f"""Weather forecast for {city} at {tm}
     Overcast:        {overcast}
     Temperature:     {temp}°{temp_unit} -- {temp_feel}°{temp_unit}
     Min Temperature: {temp_min}°{temp_unit} -- Max Temperature: {temp_max}°{temp_unit}
     Humidity:        {humidity}  
-    Wind speed:      {wnd_speed}
-
+    Wind speed:      {wnd_speed} m/s
+    Sunrise:         {sunrise}  
+    Sunset:          {sunset}
     """
     print(res)
 
